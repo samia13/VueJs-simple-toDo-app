31e24bfe16478355531de520543dd9caf59dce9d
Samia Mahi <mahisamia13@gmail.com>
2020-03-04 09:39:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-
+		var app = new Vue({
+			el :"#app",
+			data :{
+ 				newTask :{
+			        description: '',
+			        completed: false
+			    },
+				tasks :[
+					{ id : 1 , description :'Lean vue js', completed : true },
+					{ id : 2 , description :'Master css', completed : false },
+					{ id : 3 , description :'Master laravel', completed : false },
+					{ id : 4 , description :'Master Apis', completed : false }
+				]
+			},
+			methods :{
+				addTask(){
+					this.tasks.push({id : ++this.tasks.length , description : this.newTask.description , completed : false});
+					this.newTask.description = '';
+				},
+				toggleClass(task){
+					task.completed = !task.completed ;
+				}
+			},
+			computed :{
+				filterCompletedTasks(){
+					return this.tasks.filter(task => task.completed);
+				},
+				filterIncompletedTasks(){
+					return this.tasks.filter(task => !task.completed);
+				}
+			}
+		})
